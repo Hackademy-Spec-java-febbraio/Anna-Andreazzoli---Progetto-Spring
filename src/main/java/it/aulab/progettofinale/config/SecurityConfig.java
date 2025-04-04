e617738bd76d824c18452c9a474c93098f0fefe4
Anna-Git-97 <annaandreazzoli@gmail.com>
2025-04-04 11:43:20 +0200

    accesso a tutti gli utenti e non solo agli utenti autenticati e e i permessi di visualizzazione anche a qualsiasi risorsa statica presente all’interno delfolder images

--- src/main/java/it/aulab/progettofinale/config/SecurityConfig.java
+++ src/main/java/it/aulab/progettofinale/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/register/**").permitAll()
-                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/register", "/", "/articles", "/images/**").permitAll() //permette l'accesso a tutti gli utenti e non solo agli utenti autenticati e e i permessi di visualizzazione anche a qualsiasi risorsa statica presente all’interno delfolder “images“
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login")
                         .loginProcessingUrl("/login")
