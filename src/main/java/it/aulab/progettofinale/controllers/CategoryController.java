73bcb7e70d7ff11c9b84b57657d058bd9c3227ca
Anna-Git-97 <annaandreazzoli@gmail.com>
2025-04-08 17:22:26 +0200

    completato tutto il crud per le categorie

--- src/main/java/it/aulab/progettofinale/controllers/CategoryController.java
+++ src/main/java/it/aulab/progettofinale/controllers/CategoryController.java
@@ -6,9 +6,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 
 import it.aulab.progettofinale.dtos.ArticleDto;
@@ -16,6 +21,8 @@ import it.aulab.progettofinale.dtos.CategoryDto;
 import it.aulab.progettofinale.services.ArticleService;
 import it.aulab.progettofinale.services.CategoryService;
 import it .aulab.progettofinale.models.Category;
+import jakarta.validation.Valid;
+
 
 @Controller
 @RequestMapping("/categories")
@@ -31,7 +38,7 @@ public class CategoryController {
     private ModelMapper modelMapper;
 
     //Rotta per la ricerca dell'articolo in base alla categoria
-    @GetMapping
+    @GetMapping("/search/{id}")
     public String categorySearch(@PathVariable("id") Long id, Model viewModel) {
         CategoryDto category = categoryService.read(id);
 
@@ -40,6 +47,74 @@ public class CategoryController {
         List<ArticleDto> articles = articleService.searchByCategory(modelMapper.map(category, Category.class));
         viewModel.addAttribute("articles", articles);
 
-        return "articles/articles";
+        return "article/articles";
+    }
+
+    //rotta per creazione di una categoria
+    @GetMapping("create")
+    public String categoryCreate(Model viewModel){
+        viewModel.addAttribute("title", "Crea una categoria");
+        viewModel.addAttribute("category", new Category());
+        return "category/create";
+        
+    }
+
+    //Rotta per la memorizzazione di una categoria
+    @PostMapping
+    public String categoryStore(@Valid @ModelAttribute("category") Category category, 
+                                BindingResult result, 
+                                RedirectAttributes redirectAttributes, 
+                                Model viewModel) {
+
+        //Controllo degli errori con validazioni
+        if (result.hasErrors()) {
+            viewModel.addAttribute("title", "Crea una categoria");
+            viewModel.addAttribute("category", category);
+            return "category/create";
+        }
+
+        categoryService.create(category, null, null);
+        redirectAttributes.addFlashAttribute("successMessage", "Categoria aggiunta con successo");
+
+        return "redirect:/admin/dashboard";
+    }
+
+    //Rotta per la modifica di una categoria
+    @GetMapping("/edit/{id}")
+    public String categoryEdit(@PathVariable("id") Long id, Model viewModel) {
+        viewModel.addAttribute("title", "Modifica categoria");
+        viewModel.addAttribute("category", categoryService.read(id));
+        return "category/update";
+    }
+
+    //rotta per la memorizzazione delle modifiche di una categoria
+    @PostMapping("/update/{id}")
+    public String categoryUpdate(@PathVariable("id") Long id,
+                                 @Valid @ModelAttribute("category") Category category,
+                                 BindingResult result,
+                                 RedirectAttributes redirectAttributes,
+                                 Model viewModel) {
+    
+        //Controllo degli errori con validazioni 
+        if (result.hasErrors()) {
+            viewModel.addAttribute("title", "Modifica categoria");
+            viewModel.addAttribute("category", category);
+            return "category/update";
+        }
+
+        categoryService.update(id, category, null);
+        redirectAttributes.addFlashAttribute("successMessage", "Categoria modificata con successo");
+
+        return "redirect:/admin/dashboard";
+    }
+
+    //rotta per cancellazione della categoria
+    @GetMapping("/delete/{id}")
+    public String categoryDelete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+    
+        categoryService.delete(id);
+        redirectAttributes.addFlashAttribute("successMessage", "Categoria eliminata con successo");
+
+        return "redirect:/admin/dashboard";
     }
 }
--- src/main/java/it/aulab/progettofinale/services/CategoryService.java
+++ src/main/java/it/aulab/progettofinale/services/CategoryService.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.stereotype.Service;
 
 import it.aulab.progettofinale.dtos.CategoryDto;
 import it.aulab.progettofinale.repositories.CategoryRespository;
+import it.aulab.progettofinale.models.Article;
 import it.aulab.progettofinale.models.Category;
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoryService implements CrudService<CategoryDto, Category, Long>{
@@ -37,11 +41,38 @@ public class CategoryService implements CrudService<CategoryDto, Category, Long>
     }
 
     @Override
-    public CategoryDto create(Category model, Principal principal, MultipartFile file) {return null;}
+    public CategoryDto create(Category model, Principal principal, MultipartFile file) {
+        return modelMapper.map(categoryRepository.save(model), CategoryDto.class);
+    }
 
     @Override
-    public CategoryDto update(Long key, Category model, MultipartFile file) {return null;}
+    public CategoryDto update(Long key, Category model, MultipartFile file) {
+        if(categoryRepository.existsById(key)){
+            model.setId(key);
+            return modelMapper.map(categoryRepository.save(model), CategoryDto.class);
+            } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
 
+    @Transactional
     @Override
-    public void delete(Long key) {}
+    public void delete(Long id) {
+        if(categoryRepository.existsById(id)){
+
+            Category category = categoryRepository.findById(id).get();
+            
+            if (category.getArticles() != null) {
+                Iterable<Article> articles = category.getArticles();
+                for (Article article : articles) {
+                    article.setCategory(null);
+                }
+            }
+            
+            categoryRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+                
+        }
+    }
 }
