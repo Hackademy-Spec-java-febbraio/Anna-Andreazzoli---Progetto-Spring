aef78e8d0eb7f2ad07c9b284f44bcc36bebd656c
Anna-Git-97 <annaandreazzoli@gmail.com>
2025-04-17 18:41:56 +0200

    ultimo commit

--- src/main/java/it/aulab/progettofinale/config/SecurityConfig.java
+++ src/main/java/it/aulab/progettofinale/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/admin/dashboard", "/categories/create","/categories/edit/{id}", "/categories/update/{id}", "/categories/delete/{id}").hasRole("ADMIN")
                         .requestMatchers("/revisor/dashboard", "/revisor/detail/{id}", "/accept").hasRole("REVISOR")
                         .requestMatchers("/writer/dashboard", "/articles/create", "/articles/edit/{id}", "/articles/update/{id}", "/articles/delete/{id})").hasRole("WRITER")
-                        .requestMatchers("/register", "/", "/articles", "/images/**", "articles/detail/**","/categories/search/{id}", "/search/{id}", "/articles/search" ).permitAll() //permette l'accesso a tutti gli utenti e non solo agli utenti autenticati e e i permessi di visualizzazione anche a qualsiasi risorsa statica presente all’interno delfolder “images“
+                        .requestMatchers("/register", "/", "/articles", "/images/**", "/articles/detail/**","/categories/search/{id}", "/search/{id}", "/articles/search" ).permitAll() //permette l'accesso a tutti gli utenti e non solo agli utenti autenticati e e i permessi di visualizzazione anche a qualsiasi risorsa statica presente all’interno delfolder “images“
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login")
                         .loginProcessingUrl("/login")
